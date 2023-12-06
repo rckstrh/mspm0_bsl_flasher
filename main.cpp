4ad680e5366b55c62af70b722e89f4ef005db21a
Jonas Rockstroh <jonas.rockstroh@pionix.de>
2023-12-06 18:04:14 +0100

    most simple cli arguments handling serialport and path to binary only

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "bsl_tool.h"
 
-int main(int, char**){
+void print_usage();
+
+int main(int argc, char** argv){
     printf("MSPM0 BSL UART Flashing utility\n\n");
 
-    auto b = BSLTool("/dev/ttyACM0");
-    //b.flash_image("/home/j/blinky_workspace_v12/blink_led_LP_MSPM0G3507_freertos_ticlang/Debug/blink_led_LP_MSPM0G3507_freertos_ticlang.bin");
-    b.flash_image("/home/j/customers/Phytec/phyVERSO/phyverso-firmware/phyverso-firmware/Debug/phyverso-firmware.bin");
+    if(argc < 3) {
+        print_usage();
+        return -1;
+    }
+
+    const char* serial_path = argv[1];
+    const char* file_path = argv[2];
+
+    printf("Using serial %s to flash %s\n\n", serial_path, file_path);
+
+    auto b = BSLTool(serial_path);
+    bool status = b.flash_image(file_path);
+
+    return status;
+}
+
+void print_usage()
+{
+    printf("Usage: MSPM0_bsl_flasher SERIAL pathToBinary\n=>Example: MSPM0_bsl_flasher /dev/ttyACM0 /home/foo/bar.bin\n\n");
 }
